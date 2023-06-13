a75ce94fb6a9c882631e5d19a487dea9bb1ef8cd
Purshottam <86057902+psrp7@users.noreply.github.com>
2023-06-13 17:11:03 +0530

    Update index.php

--- EXP 9/index.php
+++ EXP 9/index.php
@@ -10,16 +10,10 @@
 <?php 
     $name = $email = $gender = $comment = $num = $age ="";
 
-    if($_SERVER["REQUEST_METHOD"]=="POST" && !empty($_POST["gender"]) == true  && !empty($_POST["age"]) == true)
+    if($_SERVER["REQUEST_METHOD"]=="POST"  && !empty($_POST["num"]) == true  && !empty($_POST["name"]))
     {
 
-          if(empty($_POST["name"]))
-          {
-            echo "Enter a atleast a charcter ";
-            echo "<br>";
-           }
-          else
-          {              
+                     
             if((preg_match ("/^[a-zA-z]*$/", $name))  == true)
             {        
                 $name = test_input($_POST["name"]);
@@ -29,15 +23,47 @@
                 echo "Enter a valid name";
                 echo "<br>";
             }
-          }
-          
-      
+        
         $email = test_input($_POST["email"]);
-        $gender = test_input($_POST["gender"]);
         $comment = test_input($_POST["comment"]);
-        $num = test_input($_POST["num"]);
-        $age = test_input($_POST["age"]);
-    }else{
+
+        if(!empty($_POST["gender"]) == true)
+        $gender = test_input($_POST["gender"]);
+     else
+        echo "Please kindly enter your age ";
+       
+
+
+
+    
+         if ($_POST["num"] > 999999999 && $_POST["num"] <= 9999999999    )
+            $num = test_input($_POST["num"]);
+          else
+            echo "this number is not valid the number should be 10 digit only ";
+        
+
+
+            if( !empty($_POST["age"]) == true){
+            
+            }
+               
+
+            if( !empty($_POST["age"]) == true)
+            {
+                if($_POST["age"]  > 0 && $_POST["age"] < 150)
+           
+                $age = test_input($_POST["age"]);
+             else {
+                echo "Enter a Valid age";
+              }
+          }else{
+            echo "Please enter your age ";
+          }
+               
+        
+    }
+     
+    else{
      
         echo "Please Fill the required info.";
         echo "<br>"; echo "<br>"; echo "<br>"; echo "<br>";
@@ -51,37 +77,49 @@
         return $data;
     }
 ?>
-
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
+<style>
+    .div1{
+        background-color:rgb(245, 217, 217);
+      margin: 5rem;
+      margin-left: 300px;
+      margin-right: 45%;
+      padding:2rem;
+      border:solid black 2px;
+    }
+</style>
+<div class="div1">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
     
-        <label for="name">Name: </label>
-        <input type="text" name="name">
-        <br>
-        <label for="email">Email: </label required>
-        <input type="Email" name="email">
-        <br>
-        Gender:
-        <input type="radio" name="gender" value="male">
-        <label for="male">Male</label>
-        <input type="radio" name="gender" value="female">
-        <label for="female">Female</label>
-        <br>
-        <label for="comment">Comment: </label>
-        <textarea name="comment" cols="30" rows="10"></textarea>
-        <br>
-        <label for="number">Phone: </label>
-        <input type="number" name="num">
-        <br>
-        <label for="age">AgSe: </label>
-        <input type="number" name="age">
-        <br>
-        <input type="submit">
+    <label for="name">Name: </label>
+    <input type="text" name="name">
+    <br>
+    <label for="email">Email: </label required>
+    <input type="Email" name="email">
+    <br>
+    Gender:
+    <input type="radio" name="gender" value="male">
+    <label for="male">Male</label>
+    <input type="radio" name="gender" value="female">
+    <label for="female">Female</label>
+    <br>
+    <label for="comment">Comment: </label>
+    <textarea name="comment" cols="30" rows="10"></textarea>
+    <br>
+    <label for="number">Phone: </label>
+    <input type="number" name="num">
+    <br>
+    <label for="age">Age: </label>
+    <input type="number" name="age">
+    <br>
+    <input type="submit">
 
-        <?php 
-        echo 'psrp'.$name.''; // img '' at end
-        echo ' <h2>Your response</h2> Name : '. $name .'<br>Email : ' . $email . '<br>Gender: ' .$gender. '<br>Comment : ' .$comment. '<br>phone : ' .$num.'<br>age : '.$age.'';
-        ?>
-        
-    </form>
+    <?php 
+   
+    echo ' <h2>Your response</h2> Name : '. $name .'<br>Email : ' . $email . '<br>Gender: ' .$gender. '<br>Comment : ' .$comment. '<br>phone : ' .$num.'<br>age : '.$age.'';
+    ?>
+    
+</form>
+</div>
+   
 </body>
 </html>
